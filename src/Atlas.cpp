16924c29090028df327a7deee394f49e7201cd9a
Troy Gilman <troygilman@gmail.com>
2023-04-20 16:31:35 -0400

    attempting to implement camera movement

--- src/Atlas.cpp
+++ src/Atlas.cpp
@@ -119,6 +119,55 @@ void Atlas::start() {
     }
 }
 
+void handleCamera(Entity::EntityState* entityState, Input* input) {
+    Entity::Entity* camera = entityState->entities[1];
+
+    if (input->isKeyPressed(GLFW_KEY_W)) {
+        camera->spatial3D_Position.z += 0.1f;
+    }
+    if (input->isKeyPressed(GLFW_KEY_S)) {
+        camera->spatial3D_Position.z -= 0.1f;
+    }
+    if (input->isKeyPressed(GLFW_KEY_A)) {
+        camera->spatial3D_Position.x += 0.1f;
+    }
+    if (input->isKeyPressed(GLFW_KEY_D)) {
+        camera->spatial3D_Position.x -= 0.1f;
+    }
+    if (input->isKeyPressed(GLFW_KEY_SPACE)) {
+        camera->spatial3D_Position.y -= 0.1f;
+    }
+    if (input->isKeyPressed(GLFW_KEY_LEFT_SHIFT)) {
+        camera->spatial3D_Position.y += 0.1f;
+    }
+
+    f32 dx = (f32) input->getMouseDeltaX();
+    f32 dy = (f32) input->getMouseDeltaY();
+
+    f32 mouseSensitivity = 0.15f;
+    f32 verticalViewAngle = 90.0f;
+
+    if (camera->camera_VerticalAngle - dy * mouseSensitivity > verticalViewAngle ||
+        camera->camera_VerticalAngle - dy * mouseSensitivity < -verticalViewAngle) {
+        camera->camera_VerticalAngle = camera->spatial3D_Rotation.x;
+    } else {
+        camera->camera_VerticalAngle -= dy * mouseSensitivity;
+    }
+
+    f32 horizontalDistance = (f32) cos(camera->camera_VerticalAngle * M_PI / 180.0f);
+    f32 verticalDistance = (f32) sin(camera->camera_VerticalAngle * M_PI / 180.0f);
+
+    f32 objectRotationY = camera->spatial3D_Rotation.y;
+    f32 xOffset = (f32) (horizontalDistance * sin(objectRotationY * M_PI / 180.0f));
+    f32 zOffset = (f32) (horizontalDistance * cos(objectRotationY * M_PI / 180.0f));
+
+    camera->spatial3D_Position.x -= xOffset;
+    camera->spatial3D_Position.y += verticalDistance;
+    camera->spatial3D_Position.z -= zOffset;
+
+    camera->spatial3D_Rotation = { camera->camera_VerticalAngle, 180 - objectRotationY, 0.0f };
+}
+
 void Atlas::render() {
     while (!Render::shouldCloseWindow(window)) {
         input->update();
@@ -126,24 +175,7 @@ void Atlas::render() {
             Render::closeWindow(window);
             return;
         }
-        if (input->isKeyPressed(GLFW_KEY_W)) {
-            entityState->entities[1]->spatial3D_Position.z += 0.1f;
-        }
-        if (input->isKeyPressed(GLFW_KEY_S)) {
-            entityState->entities[1]->spatial3D_Position.z -= 0.1f;
-        }
-        if (input->isKeyPressed(GLFW_KEY_A)) {
-            entityState->entities[1]->spatial3D_Position.x += 0.1f;
-        }
-        if (input->isKeyPressed(GLFW_KEY_D)) {
-            entityState->entities[1]->spatial3D_Position.x -= 0.1f;
-        }
-        if (input->isKeyPressed(GLFW_KEY_SPACE)) {
-            entityState->entities[1]->spatial3D_Position.y -= 0.1f;
-        }
-        if (input->isKeyPressed(GLFW_KEY_LEFT_SHIFT)) {
-            entityState->entities[1]->spatial3D_Position.y += 0.1f;
-        }
+        handleCamera(entityState, input);
         Render::render(window, assetPack, entityState);
     }
     Render::closeWindow(window);
